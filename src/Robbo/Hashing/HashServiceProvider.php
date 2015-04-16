7f5ec1e44ff183cf09a19244657a84b34ddf7ba7
David Philip Gösenbauer <david.goesenbauer@gmail.com>
2015-04-16 13:52:15 +0200

    Add: WpHasher

--- src/Robbo/Hashing/HashServiceProvider.php
+++ src/Robbo/Hashing/HashServiceProvider.php
@@ -4,13 +4,13 @@ use Illuminate\Hashing\HashServiceProvider as ServiceProvider;
 
 class HashServiceProvider extends ServiceProvider {
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['hash'] = $this->app->share(function() { return new Sha512Hasher; });
-	}
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->app['hash'] = $this->app->share(function() { return new WpHasher; });
+  }
 }
